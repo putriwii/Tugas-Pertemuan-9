d5696495bed4a1b88e298637965ce251f2f5bce3
putriwii <putri32rizky@gmail.com>
2024-11-19 13:17:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$nama   = "Putri Rizky Pratiwi";
+$Kelas  = "TI.23.CA.1";
+$Nim    = "312310554";
+$Prodi  = "Tekhnik Informatika";
+
+?>
+
+<h1>
+Saya Adalah 
+<?php echo $nama?>
+</h1>
+<p>
+ <strong>Kelas</strong>=<?php echo $Kelas?>
+</p>
+<p>
+ <strong>Nim</strong>=<?php echo $Nim?>
+</p>
+<p>
+<strong>Prodi</strong>=<?php echo $Prodi?>
+</p>
